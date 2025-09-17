fbc5c2000e85fd7c4e71b7dbfc74ada09dccf0f6
Gustavo Kissel <kisselgustavo@gmail.com>
2025-09-17 14:43:06 -0300

    Refactor metadata.ts: improve formatting for consistency and readability Add image-processing utility functions for WebP conversion and image blurring

--- apps/api/src/metadata.ts
+++ apps/api/src/metadata.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
 export default async () => {
-    const t = {};
-    return { "@nestjs/swagger": { "models": [], "controllers": [] } };
+  const t = {};
+  return { "@nestjs/swagger": { models: [], controllers: [] } };
 };
